0ca61a3f11803a55b839a424682f9cce081d8289
KhramtsovMykhaylo <mykhaylokhramtsov@gmail.com>
2025-12-02 11:05:57 +0100

    domain layer implemented

---
+++ src/main/java/app/domain/Product.java
@@ -0,0 +1,32 @@
+package app.domain;
+
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String title;
+    private double price;
+    private boolean active;
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Product product = (Product) object;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product: id - %d, title -%s, price - %.2f, active -%s",
+                id, title, price, active ? "yes" : "no");
+    }
+}
